59682795b9f791880ec4014a92ecb31084e5768b
Talha Khalid <hafiz.talhakh@gmail.com>
2025-02-17 11:53:06 +0500

    Static code implementation done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// Import the App component
+import App from './App.jsx';
+
+// Import the cartReducer
+import cartReducer from './state';
+
+// Import the theme
+import { theme } from './theme';
+
+// Globall styles
+import './index.css';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </StrictMode>
+);
